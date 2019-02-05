c7facd25a579afc2bd1c0c8776dc129f75441890
Martin <stefanik.mar@gmail.com>
2019-02-05 17:06:52 +0100

    commented out compton startup

--- .config/plasma-workspace/env/wm.sh
+++ .config/plasma-workspace/env/wm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Disable kwin and use i3-gaps as a window manager
-export KDEWM=/usr/bin/i3
+# export KDEWM=/usr/bin/i3
 
 # Start the compton compositor
-compton & --config ~/.config/compton/compton.conf
+# compton & --config ~/.config/compton/compton.conf
